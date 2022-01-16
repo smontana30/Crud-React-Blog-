e27afab2d317565c29a6e920535782d97fd19748
smontana30 <smontana@knights.ucf.edu>
2022-01-15 22:50:06 -0500

    crud blog app

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -11,7 +11,7 @@ import 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  YOUR_OWN_FIREBASE_CONFIG
+  YOUR_OWN_CONFIG
 };
 
 // Initialize Firebase
